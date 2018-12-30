b0a144b7e3be8d8acef38e0dd07b4f7e0ddd0203
microhhh <13654840887@163.com>
2018-12-30 21:58:33 +0800

    interpolation

--- interpolation.py
+++ interpolation.py
@@ -3,6 +3,28 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.interpolate import *
 
+
+def lagrange_interpolation(x, y, degree):
+    n = degree + 1
+    L = np.ones(6, dtype=np.float64)
+    for i in range(n):
+        for j in range(n):
+            if j != i:
+                L[0] = L[0] / (x[i] - x[j])
+    L = L * y
+    return L
+
+
+def _lagrange(t, x0, L):
+    n = len(L)
+    for i in range(n):
+        for j in range(n):
+            if j != i:
+                L[0] = L[0] * (t - x0[0])
+
+    return np.sum(L)
+
+
 if __name__ == '__main__':
     x = np.array([0.0, 0.5, 1.0, 6.0, 7.0, 9.0], dtype=np.float64)
     y = np.array([0.0, 1.6, 2.0, 1.5, 1.5, 0.0], dtype=np.float64)
@@ -15,6 +37,9 @@ if __name__ == '__main__':
     t = np.linspace(0, 10, 1000)
     c5 = f5(t)
     c3 = splev(t, tck)
+
+    # l = lagrange_interpolation(x, y, 5)
+    # _lagrange(t, x, l)
 
     plt.rcParams['font.sans-serif'] = ['SimHei']
     plt.plot(x, y, color='m', linestyle='', marker='o', label='给定数据')
---
+++ test.py
@@ -0,0 +1,6 @@
+# -*- coding: UTF-8 -*-
+import numpy as np
+
+if __name__ == '__main__':
+    y = np.zeros(5, dtype=np.float64)
+    print(y)
